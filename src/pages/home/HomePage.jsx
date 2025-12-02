5bd186170b5133b9629265917b8593a717127e36
almanbetzhorobekov <Almanbet.Zhorobekov@veda.net>
2025-12-02 11:09:31 +0100

    With-react-query

--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -1,11 +1,19 @@
 import Hero from "./components/Hero";
 import Cards from "./components/Cards";
+import Header from "../layout/components/Header.jsx";
+import Footer from "../layout/components/Footer.jsx";
 
 export default function HomePage() {
   return (
-    <>
-      <Hero />
-      <Cards />
-    </>
+      <>
+
+      <Header />
+      <main>
+        <Hero />
+        <Cards />
+      </main>
+      <Footer />
+      
+      </>
   );
 }
