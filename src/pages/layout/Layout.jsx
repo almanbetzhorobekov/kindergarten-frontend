efe1cac9ea4c84caa5ca4413a78a214110c9ff83
almanbetzhorobekov <Almanbet.Zhorobekov@veda.net>
2025-12-24 22:52:20 +0100

    last commit mit UI

--- src/pages/layout/Layout.jsx
+++ src/pages/layout/Layout.jsx
@@ -3,7 +3,7 @@ import Footer from "./components/Footer";
 import { Outlet } from "react-router-dom";
 import { Box } from "@mui/material";
 
-export default function Layout({ children }) {
+export default function Layout() {
   return (
     <Box
       sx={{
@@ -12,21 +12,24 @@ export default function Layout({ children }) {
         flexDirection: "column",
       }}
     >
-      <>
-        <Header />
-        <Outlet />
-        {/** Main  */}
-        <Box
-          component="main"
-          sx={{
-            flexGrow: 1,
-            p: 3,
-          }}
-        >
-          {children}
-        </Box>
-        <Footer />
-      </>
+      {/* Header */}
+      <Header />
+
+      {/* Main content */}
+      <Box
+        component="main"
+        sx={{
+          flexGrow: 1,
+          py: 4,
+        }}
+      >
+        <Container maxWidth="lg">
+          <Outlet />
+        </Container>
+      </Box>
+
+      {/* Footer */}
+      <Footer />
     </Box>
   );
 }
