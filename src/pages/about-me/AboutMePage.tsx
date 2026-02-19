99b4477231b18997bd69e686a55eb6df61b8effc
almanbetzhorobekov <Almanbet.Zhorobekov@veda.net>
2026-02-19 14:13:26 +0100

    ..

---
+++ src/pages/about-me/AboutMePage.tsx
@@ -0,0 +1,14 @@
+import { Box, Typography } from "@mui/material";
+
+export default function AboutMePage() {
+  return (
+    <Box>
+      <Box>
+        <Typography variant="h1">
+          Hier kann man ein Admin Panel erstellen!
+        </Typography>
+        <Typography>ohne link</Typography>
+      </Box>
+    </Box>
+  );
+}
